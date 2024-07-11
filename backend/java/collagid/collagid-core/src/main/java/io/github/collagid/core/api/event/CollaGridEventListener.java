bd025212e2c2af7019c3446aca5c57a6d6ec07e6
ziqiang <1694392889@qq.com>
2024-07-11 09:47:31 +0800

    feat(core): get data api

--- backend/java/collagid/collagid-core/src/main/java/io/github/collagid/core/api/event/CollaGridEventListener.java
+++ backend/java/collagid/collagid-core/src/main/java/io/github/collagid/core/api/event/CollaGridEventListener.java
@@ -5,6 +5,5 @@ import io.github.collagid.core.api.record.event.RecordCreatedEvent;
 public interface CollaGridEventListener<T extends CollaGridEvent>  {
     void handle(T event);
 
-
     CollaGridEventType getType();
 }
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,69 +1,40 @@
-import {ClientSideRowModelModule, createGrid, GridApi, GridOptions, ModuleRegistry} from "ag-grid-community";
+import { ClientSideRowModelModule, createGrid, GridApi, GridOptions, ModuleRegistry } from "ag-grid-community";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
-// Row Data Interface
+import { getData } from "./api";
+
 interface IRow {
     recordId?: string;
     [key: string]: any;
 }
 
-interface CollaGridOption{
+interface CollaGridOption {}
 
-}
-
-// CollaGrid Class
 class CollaGrid {
     private gridApi: GridApi | undefined;
     private gridOptions: GridOptions<IRow>;
-    private instance: Map<string, IRow>;
+    private recordMap: any;
 
     constructor(option: CollaGridOption) {
         console.log('option is', option);
-        this.instance = new Map<string, IRow>([
-            ["asx", { make: "asx", model: "Model Y", price: 64950, electric: true }],
-            ["Tesla", { make: "Tesla", model: "Model Y", price: 64950, electric: true }],
-            ["Ford", { make: "Ford", model: "F-Series", price: 33850, electric: false }],
-            ["Toyota", { make: "Toyota", model: "Corolla", price: 29600, electric: false }],
-            ["Mercedes", { make: "Mercedes", model: "EQA", price: 48890, electric: true }],
-            ["Fiat", { make: "Fiat", model: "500", price: 15774, electric: false }],
-            ["Nissan", { make: "Nissan", model: "Juke", price: 20675, electric: false }]
-        ]);
-
         this.gridOptions = {
-            rowData: [
-                { recordId: "Tesla" },
-                { recordId: "Ford" },
-                { recordId: "Toyota" },
-                { recordId: "Mercedes" },
-                { recordId: "Fiat" },
-                { recordId: "Nissan" }
-            ],
-            columnDefs: [
-                { field: "make", enableCellChangeFlash: true,
-                    valueGetter: (p) => this.getValue(p, "make") },
-                { field: "model", valueGetter: (p) => this.getValue(p, "model") },
-                { field: "price", valueGetter: (p) => this.getValue(p, "price") },
-                { field: "electric", valueGetter: (p) => this.getValue(p, "electric") }
-            ],
-            getRowId: (data) => {
-                return data.data.recordId as string;
+            rowData: [],
+            onRowClicked: (params: any) => {
+                const val = this.recordMap[params.data.recordId];
+                console.log(`create record id is ${val.createV} and v is: ${val.v} ${val.id}`);
             },
+            columnDefs: [],
+            getRowId: (data) => data.data.recordId as string,
             defaultColDef: {
                 flex: 1
             }
         };
     }
 
-    private getValue(params: any, field: string) {
-        const dataMap = this.instance;
-        const rowData = dataMap.get(params.data.recordId);
-        return rowData ? rowData[field] : undefined;
-    }
-
-    private refreshGrid() {
-        if (this.gridApi) {
-            this.gridApi.refreshCells({ force: true });
-        }
+    private getValue(params: any) {
+        const fieldId = params.colDef.field;
+        const recordId = params.data.recordId;
+        return this.recordMap[recordId].data[fieldId][0].text;
     }
 
     private refreshSpecificCell(recordId: string, field: string) {
@@ -78,45 +49,44 @@ class CollaGrid {
         }
     }
 
-    // Method to update map and refresh grid
-    updateData(recordId: string, newData: Partial<IRow>) {
-        const dataMap = this.instance;
-        const existingData = dataMap.get(recordId);
-        if (existingData) {
-            dataMap.set(recordId, { ...existingData, ...newData });
-            // this.refreshGrid();
-            this.refreshSpecificCell(recordId, 'make');
-        }
-    }
-
-
     // Initialize the grid
     mount(domId: string) {
-        console.log('xx', Array.from(this.instance.keys()));
-        // Register modules
+        getData('dst1dnr6blpf7gubsr').then((data) => {
+            console.log('zzq see', data);
+            const columns = data.data.meta.views[0].columns;
+            const fieldMap = data.data.meta.fieldMap;
+            const headers: any[] = [{
+                headerName: '#',
+                field: 'rowIndex',
+                minWidth: 60,
+                sortable: false,
+                filter: false,
+                resizable: false,
+                valueGetter: (params: any) => params.node.rowIndex + 1
+            }];
+            this.recordMap = data.data.recordMap;
+            columns.forEach((column: any) => {
+                headers.push({
+                    field: column.fieldId,
+                    minWidth: 160,
+                    headerName: fieldMap[column.fieldId].name,
+                    valueGetter: this.getValue.bind(this)
+                });
+            });
+            const rows = data.data.meta.views[0].rows;
+            this.gridOptions.rowData = rows;
+            this.gridOptions.columnDefs = headers;
+            console.log('zzq see rows', this.gridOptions);
+            this.gridApi = createGrid(
+                document.querySelector<HTMLElement>(domId)!,
+                this.gridOptions
+            );
+        });
         ModuleRegistry.registerModules([ClientSideRowModelModule]);
-        console.log('xxmsms');
-        // Create Grid
-        this.gridApi = createGrid(
-            document.querySelector<HTMLElement>(`${domId}`)!,
-            this.gridOptions
-        );
-        setTimeout(() => {
-            this.updateData("Tesla", { make: 'xxx'});
-        }, 3000);
-        setTimeout(() => {
-            const newRowData = [
-                {recordId: "asx"}
-            ];
-            this.gridOptions.rowData = newRowData;
-            this.gridApi?.applyTransaction({ add: newRowData });
-        }, 5000);
     }
 }
 
-const createCollaGrid = (option: CollaGridOption) => {
-    return new CollaGrid(option);
-}
+const createCollaGrid = (option: CollaGridOption) => new CollaGrid(option);
 
 /**
  * Adds two numbers together.
@@ -133,6 +103,5 @@ function add(a: number, b: number): number {
     return a + b;
 }
 
-export {createCollaGrid, CollaGrid, add};
+export { createCollaGrid, CollaGrid, add };
 export type { CollaGridOption };
-
